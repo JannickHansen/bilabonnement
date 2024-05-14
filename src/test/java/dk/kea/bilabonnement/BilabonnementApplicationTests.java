88b4f170aa34ac4382b5e6d7e8cf93e0ed86b3a3
tgavri <thom54s8@stud.kea.dk>
2024-05-14 13:54:00 +0200

    init

---
+++ src/test/java/dk/kea/bilabonnement/BilabonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.bilabonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilabonnementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
