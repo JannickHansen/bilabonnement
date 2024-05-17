5617affcb73f413b79485d564e0e4a353cb1720c
tgavri <thom54s8@stud.kea.dk>
2024-05-17 10:20:32 +0200

    Skade og register.html, style rettet.

---
+++ src/main/java/dk/kea/bilabonnement/model/Lejeaftale.java
@@ -0,0 +1,59 @@
+package dk.kea.bilabonnement.model;
+
+import java.time.LocalDate;
+
+public class Lejeaftale {
+    LocalDate pickupdate;
+    LocalDate pickuptime;
+    String renttype;
+    String pickuplocation;
+    Bil bil;
+
+    public Lejeaftale(LocalDate pickupdate, LocalDate pickuptime, String renttype, String pickuplocation, Bil bil) {
+        this.pickupdate = pickupdate;
+        this.pickuptime = pickuptime;
+        this.renttype = renttype;
+        this.pickuplocation = pickuplocation;
+        this.bil = bil;
+    }
+
+    public LocalDate getPickupdate() {
+        return pickupdate;
+    }
+
+    public LocalDate getPickuptime() {
+        return pickuptime;
+    }
+
+    public String getRenttype() {
+        return renttype;
+    }
+
+    public String getPickuplocation() {
+        return pickuplocation;
+    }
+
+    public Bil getBil() {
+        return bil;
+    }
+
+    public void setPickupdate(LocalDate pickupdate) {
+        this.pickupdate = pickupdate;
+    }
+
+    public void setPickuptime(LocalDate pickuptime) {
+        this.pickuptime = pickuptime;
+    }
+
+    public void setRenttype(String renttype) {
+        this.renttype = renttype;
+    }
+
+    public void setPickuplocation(String pickuplocation) {
+        this.pickuplocation = pickuplocation;
+    }
+
+    public void setBil(Bil bil) {
+        this.bil = bil;
+    }
+}
